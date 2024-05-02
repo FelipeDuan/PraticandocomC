ef3f5f3d26fa15dde0cc2d75c52dd6e6e7db87f7
Felipe Duan <felipeduan1@gmail.com>
2024-05-02 16:06:41 -0300

    criando calculadora estilizada utilizando switch case

---
+++ Calculadora2num.C
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+int operacao, valor1, valor2, resultado, cod;
+
+//Menu da Calculadora
+void MenuCalculadora () {
+    printf("+-----------------------------+\n");
+    printf("|      CALCULADORA SIMPLES    |\n");
+    printf("+=============================+\n");
+    printf("|     1-SOMAR   |  2-SUBTRAIR +\n");
+    printf("+=============================+\n");
+    printf("| 3-MULTIPLICAR |  4-DIVIDIR  +\n");
+    printf("+=============================+\n");
+    printf("Informe a operacao desejada ou pressione 5 para sair \n");
+    scanf("%d", &operacao);
+
+}
+
+// Operações da Calculadora
+void Entrada() {
+    printf("Informe o primeiro valor: ");
+    scanf("%d", &valor1);
+    printf("Informe o segundo valor: ");
+    scanf("%d", &valor2);
+}
+
+void Somar() {
+    resultado = valor1 + valor2;
+}
+
+void Subtrair() {
+    resultado = valor1 - valor2;
+}
+
+void Multiplicar() {
+    resultado = valor1 * valor2;
+}
+
+void Dividir() {
+    resultado = valor1 / valor2;
+}
+
+void Saida() {
+    printf("O Resultado da operacao eh: %d \n", resultado);
+}
+
+// Código Principal
+int main() {
+    MenuCalculadora();
+    do {
+     switch (operacao) {
+        case 1:
+            Entrada();
+            Somar();
+            Saida();
+        break;
+        
+        case 2:
+            Entrada();
+            Subtrair();
+            Saida();
+        break;
+
+        case 3:
+            Entrada();
+            Multiplicar();
+            Saida();
+        break;
+
+        case 4:
+            Entrada();
+            Dividir();
+            Saida();
+        break;
+
+        printf("Deseja executar outra vez? Digite: 1 - Sim | 0 - Nao \n");
+        scanf("%d", &cod);}
+    } while (cod != 0);
+
+    return 0; 
+}
