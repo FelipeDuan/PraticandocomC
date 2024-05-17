d1bd529872e456e23bc798bb708ba2bcae9289d7
Felipe Duan <felipeduan1@gmail.com>
2024-05-17 14:28:37 -0300

    últimos ajustes

--- Matriz.C
+++ Matriz.C
@@ -6,15 +6,15 @@ int DL, DC, repetir;
 void Matriz() {
     int matriz[DL][DC];
     printf("Informe os valores da Matriz %dx%d \n", DL, DC);
-    for (int i = 0; i < DL; i++) {
-        for (int j = 0; j < DC; j++) {
+    for (int i = 1; i <= DL; i++) {
+        for (int j = 1; j <= DC; j++) {
             printf("\n Elemento [%d][%d]= ", i, j);
             scanf("%d", &matriz[i][j]);
         }  
     }
     printf("\nMatriz:\n");
-    for (int i = 0; i < DL; i++) {
-        for (int j = 0; j < DC; j++) {
+    for (int i = 1; i <= DL; i++) {
+        for (int j = 1; j <= DC; j++) {
             printf("| %4d ", matriz[i][j]);
         }
         printf("|\n");
